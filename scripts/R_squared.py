6db68c0d3f770d0329a01f5377f4a6a742e030bc
Arman Duha <arman.udduha774@gmail.com>
2025-02-10 18:24:31 -0600

    Update R_squared.py

--- scripts/R_squared.py
+++ scripts/R_squared.py
@@ -1,19 +1,19 @@
-'Plot Energy vs Displacement from EvsDist file'
+#Plots raw Ed vs predicted Ed from analytical formula and calculates R squared value
 import numpy as np
 import math
 import matplotlib.pyplot as plt
 
 
-filename = '/Users/valinor/Desktop/Projects/MachineLearningAppliedToDisplacementEnergies/Data/Final/Monoatomic/monoatom_Ed_avg_all.csv'
+filename = '/directory/to/file/eds_all_method.csv' # your filepath to raw data file
 Matrix = []
 xyz = open(filename, "r")
 for line in xyz:
     # Generate Matrix with the xyz data
     Matrix.append(line.split(','))
 
-'Define result function'
+#Define the analytical expression obtained from SISSO
 def fit1(density,E_ioniz,l_bond):
-    ed = 1.296178732584652e+01 + 2.729416958744306e+02 * ((float(density) / float(E_ioniz)) / (float(l_bond)**3))
+    ed = 1.296178732584652e+01 + 2.729416958744306e+02 * ((float(density) / float(E_ioniz)) / (float(l_bond)**3)) # change to the relevant analytical expression
     return ed
     
 #generate matrix to work with data
@@ -38,11 +38,8 @@ for i in range(len(ed_exp)):
 
 R2 = round(1 - SSres/SStot,5)
 
-#with package
-#r_squared = round(r2_score(ed_exp, ed_predict),4)
 
-
-'Plotting'
+#Plotting
 plt.figure(figsize=(10,10))
 plt.title('All methods data, Model dim:1', fontsize=25)   #method1
 plt.scatter(ed_predict,ed_exp, label = f'$R^2 =$ %s'%R2, marker = 'x', color = 'k')
